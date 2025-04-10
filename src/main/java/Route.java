2654a0f0558323ab88bffdf9b3beb43542cf355d
jwo107 <jwo107@uclive.ac.nz>
2025-04-11 01:30:18 +1200

    made a fuel stop calculator, temporarily finished race and season classes. made a course selector fxml

--- src/main/java/Route.java
+++ src/main/java/Route.java
@@ -1,25 +1,39 @@
 public enum Route {
-    DESERT_DRIFT("Desert Drift", 2, new RouteAttributes(0.5, 1.5, 1.2)),
-    DESERT_LONG("Desert Long",3 , new RouteAttributes(1.5, 0.6, 1)),
-    MOUNTAIN_STEEP("Mountain Steep", 2, new RouteAttributes(1, 1, 1.2)),
-    MOUNTAIN_CURVY("Mountain Curvy", 2, new RouteAttributes(0.8, 1.3, 1.2)),
-    COUNTRYSIDE_STRAIGHT("Countryside Straight", 4, new RouteAttributes(1.6, 0.5, 1.1)),
-    COUNTRYSIDE_TWISTY("Countryside Twisty", 3, new RouteAttributes(0.8, 1.3, 1.2)),
-    CITY_WIDE_LINE("City Wide Line", 5, new RouteAttributes(1.2, 0.8, 1)),
-    CITY_CUT_LINE("City Cut Line", 5, new RouteAttributes(0.8, 1.2, 1));
+    DESERT_DRIFT("Desert Drift", new RouteAttributes(0.5, 1.5, 1.2), 20),
+    DESERT_LONG("Desert Long", new RouteAttributes(1.5, 0.6, 1), 30),
+    MOUNTAIN_STEEP("Mountain Steep", new RouteAttributes(1, 1, 1.2), 15),
+    MOUNTAIN_CURVY("Mountain Curvy", new RouteAttributes(0.8, 1.3, 1.2), 20),
+    COUNTRYSIDE_STRAIGHT("Countryside Straight", new RouteAttributes(1.6, 0.5, 1.1), 50),
+    COUNTRYSIDE_TWISTY("Countryside Twisty", new RouteAttributes(0.8, 1.3, 1.2), 30),
+    CITY_WIDE_LINE("City Wide Line", new RouteAttributes(1.2, 0.8, 1), 35),
+    CITY_CUT_LINE("City Cut Line", new RouteAttributes(0.8, 1.2, 1), 34);
 
     private String routeName;
-    private int fuelStops;
     private RouteAttributes attributes;
+    private int length;
 
-    Route(String routeName, int fuelStops, RouteAttributes attributes) {
+    Route(String routeName, RouteAttributes attributes, int length) {
         this.routeName = routeName;
-        this.fuelStops = fuelStops;
         this.attributes = attributes;
+        this.length = length;
+    }
+
+    public String getRouteName() {
+        return routeName;
     }
 
     public RouteAttributes getAttributes() {
         return attributes;
     }
 
+    public int getLength() {
+        return length;
+    }
+
+    public int calculateFuelStops(Car car) {
+        double economy = car.getFuelEconomy(); // e.g., 10 means 10 km per fuel unit
+        int rangePerTank = (int) economy;
+        return (int) Math.ceil((double) this.length / rangePerTank) - 1;
+
+    }
 }
