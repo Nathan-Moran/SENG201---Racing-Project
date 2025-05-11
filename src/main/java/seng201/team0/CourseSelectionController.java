6341898254d713883310d1c340732a220fdcf4e2
jwo107 <jwo107@uclive.ac.nz>
2025-05-11 19:58:07 +1200

    merged the course and route selection controllers, added the controller into the fxml file, changed gradle to 8.10, added test code to main function

--- src/main/java/seng201/team0/CourseSelectionController.java
+++ src/main/java/seng201/team0/CourseSelectionController.java
@@ -55,4 +55,56 @@ public class CourseSelectionController {
         routeMenu.setVisible(true);
         routeMenu.setManaged(true);
     }
+        @FXML
+        private void onDesertDriftSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.DESERT_DRIFT);
+            startRace();
+        }
+
+        @FXML
+        private void onDesertLongSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.DESERT_LONG);
+            startRace();
+        }
+
+        @FXML
+        private void onMountainSteepSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.MOUNTAIN_STEEP);
+            startRace();
+        }
+
+        @FXML
+        private void onMountainCurvesSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.MOUNTAIN_CURVES);
+            startRace();
+        }
+
+        @FXML
+        private void onCountryStraightSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.COUNTRY_STRAIGHT);
+            startRace();
+        }
+
+        @FXML
+        private void onCountryTwistSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.COUNTRY_TWISTY);
+            startRace();
+        }
+
+        @FXML
+        private void onCityAlleysSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.CITY_ALLEYS);
+            startRace();
+        }
+
+        @FXML
+        private void onCityTrafficSelected(ActionEvent event) {
+            GameEnvironment.setSelectedRoute(Route.CITY_TRAFFIC);
+            startRace();
+        }
+
+        private void startRace() {
+//        ScreenNavigator.loadScreen("RaceScreen.fxml");
+        }
 }
+
--- src/main/java/seng201/team0/Main.java
+++ src/main/java/seng201/team0/Main.java
@@ -6,13 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
     @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ShopSellScene.fxml"));
+//        primaryStage.setTitle("Shop - Sell");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ShopSellScene.fxml"));
-        primaryStage.setTitle("Shop - Sell");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/courseSelector.fxml")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
