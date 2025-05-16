58e146534396321281686665467357c348916116
Nathan <nmo101@uclive.ac.nz>
2025-05-17 01:15:32 +1200

    Moved Controller logic of: - CarSelectorController - GarageController - ShopBuyController - ShopSellController - StartMenuController To new classes ControllerLogicManager and BalanceManager

--- src/main/java/seng201/team0/gui/AbstractShopController.java
+++ src/main/java/seng201/team0/gui/AbstractShopController.java
@@ -32,7 +32,6 @@ public abstract class AbstractShopController implements Initializable {
     @FXML private TableColumn<Car, Integer> fuelColumn;
 
 
-
     //Tuning Parts
     @FXML protected TableView<TuningPart> tuningPartTable;
 
