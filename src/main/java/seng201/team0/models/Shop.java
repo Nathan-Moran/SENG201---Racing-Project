2d77ef4221e601b1bc82737e5867b062ef412579
Nathan <nmo101@uclive.ac.nz>
2025-05-19 16:47:15 +1200

    Changed some car stats to try to better align with the game

--- src/main/java/seng201/team0/models/Shop.java
+++ src/main/java/seng201/team0/models/Shop.java
@@ -7,9 +7,9 @@ public class Shop extends ItemStorage {
     }
 
     public void setShopInventory() {
-        Car Toyota = new Car("Toyota Supra", 0.85, 0.85, 0.80, 23.0, 7500);
-        Car Mustang = new Car("Mustang", 0.92, 0.78, 0.80, 19.0, 7800);
-        Car Ferrari = new Car("Ferrari 458", 0.96, 0.96, 0.90, 15.0, 15000);
+        Car ToyotaSupra = new Car("Toyota Supra", 0.85, 0.85, 0.80, 23.0, 6500);
+        Car Mustang = new Car("Mustang", 0.92, 0.78, 0.80, 19.0, 6800);
+        Car Ferrari458 = new Car("Ferrari 458", 0.96, 0.96, 0.90, 15.0, 12500);
 
         TuningPart Ethanol = new TuningPart("Ethanol", 500, "\uD83D\uDCA8", 1.2);
         TuningPart SuperCharger = new TuningPart("SuperCharger", 750, "\uD83D\uDCA8", 1.5);
@@ -20,25 +20,19 @@ public class Shop extends ItemStorage {
         TuningPart RacingWheels = new TuningPart("RacingWheels", 1250, "\uD83C\uDFAE", 1.8);
 
 
-        Car DuneDrifter = new Car("Dune Drifter", 0.5, 0.8, 0.7, 22.0, 1800);            // DESERT_DRIFT
-        Car SandstormStrider = new Car("Sandstorm Strider", 0.8, 0.5, 0.8, 25.0, 2200); // DESERT_LONG
+        Car DuneDrifter = new Car("Dune Drifter", 0.5, 0.8, 0.7, 22.0, 2200);
+        Car SandstormStrider = new Car("Sandstorm Strider", 0.8, 0.5, 0.8, 25.0, 2800);
+        Car CliffClimber = new Car("Cliff Climber", 0.6, 0.5, 0.9, 20.0, 2600);
+        Car RidgeRacer = new Car("Ridge Racer", 0.5, 0.9, 0.7, 28.0, 3000);
+        Car FarmersFlyer = new Car("Farmers Flyer", 0.9, 0.4, 0.9, 30.0, 3800);
+        Car VineyardViper = new Car("Vineyard Viper", 0.6, 0.8, 0.8, 24.0, 3000);
+        Car AlleyCat = new Car("Alley Cat", 0.8, 0.5, 0.7, 23.0, 2600);
+        Car CommuterKing = new Car("Commuter King", 0.4, 0.7, 0.9, 28.0, 3300);
 
 
-        Car CliffClimber = new Car("Cliff Climber", 0.6, 0.5, 0.9, 20.0, 2500);         // MOUNTAIN_STEEP
-        Car RidgeRacer = new Car("Ridge Racer", 0.5, 0.9, 0.7, 28.0, 2800);             // MOUNTAIN_CURVES
-
-
-        Car FarmersFlyer = new Car("Farmers Flyer", 0.9, 0.4, 0.9, 30.0, 3500);         // COUNTRY_STRAIGHT
-        Car VineyardViper = new Car("Vineyard Viper", 0.6, 0.8, 0.8, 24.0, 3000);       // COUNTRY_TWISTY
-
-
-        Car AlleyCat = new Car("Alley Cat", 0.8, 0.5, 0.7, 23.0, 3300);                 // CITY_ALLEYS
-        Car CommuterKing = new Car("Commuter King", 0.4, 0.7, 0.9, 28.0, 3800);         // CITY_TRAFFIC
-
-
-        addCar(Toyota);
-//        addCar(Mustang);
-        addCar(Ferrari);
+        addCar(ToyotaSupra);
+        addCar(Mustang);
+        addCar(Ferrari458);
 
         addTuningPart(Ethanol);
         addTuningPart(SuperCharger);
