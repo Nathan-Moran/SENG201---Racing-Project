933bfc500b4deb0d3ac6a2d5a407c9673aa76dc5
Nathan <nmo101@uclive.ac.nz>
2025-05-19 16:31:46 +1200

    Added Cars that will be unlocked in the shop after the player finishes first in a race

--- src/main/java/seng201/team0/models/Shop.java
+++ src/main/java/seng201/team0/models/Shop.java
@@ -19,6 +19,27 @@ public class Shop extends ItemStorage {
         TuningPart SportWheels = new TuningPart("SportsWheels", 800, "\uD83C\uDFAE", 1.5);
         TuningPart RacingWheels = new TuningPart("RacingWheels", 1250, "\uD83C\uDFAE", 1.8);
 
+        // Starter Cars (for reference)
+        Car HondaCivicR = new Car("Honda Civic R", 0.6, 0.5, 0.7, 20.0, 1000);
+        Car MazdaMPS = new Car("Mazda MPS", 0.5, 0.7, 0.7, 20.0, 1000);
+        Car NissanZ = new Car("Nissan Z", 0.5, 0.6, 0.8, 20.0, 1000);
+
+
+        Car DuneDrifter = new Car("Dune Drifter", 0.5, 0.8, 0.7, 22.0, 1800);            // DESERT_DRIFT
+        Car SandstormStrider = new Car("Sandstorm Strider", 0.8, 0.5, 0.8, 25.0, 2200); // DESERT_LONG
+
+
+        Car CliffClimber = new Car("Cliff Climber", 0.6, 0.5, 0.9, 20.0, 2500);         // MOUNTAIN_STEEP
+        Car RidgeRacer = new Car("Ridge Racer", 0.5, 0.9, 0.7, 28.0, 2800);             // MOUNTAIN_CURVES
+
+
+        Car FarmersFlyer = new Car("Farmers Flyer", 0.9, 0.4, 0.9, 30.0, 3500);         // COUNTRY_STRAIGHT
+        Car VineyardViper = new Car("Vineyard Viper", 0.6, 0.8, 0.8, 24.0, 3000);       // COUNTRY_TWISTY
+
+
+        Car AlleyCat = new Car("Alley Cat", 0.8, 0.5, 0.7, 23.0, 3300);                 // CITY_ALLEYS
+        Car CommuterKing = new Car("Commuter King", 0.4, 0.7, 0.9, 28.0, 3800);         // CITY_TRAFFIC
+
 
         addCar(Toyota);
 //        addCar(Mustang);
